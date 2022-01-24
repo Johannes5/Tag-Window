d025de4a475dd069a927617d300f311a128184c1
ayoung <zeusandninja@gmail.com>
2022-01-24 04:23:23 -0800

    Add side effect free emoji replacement

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,10 +1,28 @@
-const titleInnerHTML = document.querySelector("title").innerHTML;
+interface ContentState {
+  title?: string;
+  observer?: MutationObserver;
+}
+
+const state: ContentState = {};
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type == "load") {
+    if (state.observer) {
+      state.observer.disconnect();
+    }
     load();
   }
 });
+
+const check = (emoji: string) => {
+  if (!state.title && document.title) {
+    state.title = document.title;
+  }
+
+  if (state.title && !document.title.includes(emoji)) {
+    document.title = `${emoji} | ${state.title}`;
+  }
+};
 
 const load = () => {
   chrome.runtime.sendMessage({ type: "getWindowId" }, (windowId) => {
@@ -13,9 +31,16 @@ const load = () => {
 
     chrome.storage.local.get([key], (item) => {
       if (item[key]) {
-        document.querySelector(
-          "title"
-        ).innerHTML = `${item[key]} | ${titleInnerHTML}`;
+        check(item[key]);
+        state.observer = new MutationObserver((mutations) => {
+          check(item[key]);
+        });
+        state.observer.observe(document.head, {
+          childList: true,
+          subtree: true,
+          attributes: false,
+          characterData: false,
+        });
       }
     });
   });
